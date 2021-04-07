5a8d1f77ca5f2788a561efd3e2653d1a11e48ef8
Malathi <malathi11@nyu.edu>
2021-04-07 07:38:40 +0530

    Update source files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
-
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Landing from './components/layout/Landing';
+import Navbar from './components/layout/Navbar';
 const App = () => (
-  <Fragment>
-    <h1>App1</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section className='container'>
+        <Switch>
+          <Route exact path='/register' component={Register} />
+          <Route exact path='/login' component={Login} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
